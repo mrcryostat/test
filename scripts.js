f18df2175785e4cc8f71466f497d04116d7d25a2
lokicat <gannonkoenig@gmail.com>
2022-05-26 22:47:13 -0500

    you're gonna have a bad time

---
+++ scripts.js
@@ -0,0 +1,5 @@
+var name = prompt("What's your name?","");
+
+document.write("<h1>Here's a better question</h1>");
+document.write("<h1>"name"<h1>");
+document.write("</h1>Do you wanna have a bad time?</h1>");
